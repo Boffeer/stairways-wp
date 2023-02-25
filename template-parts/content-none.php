981244d76b8a272698e185aa6dc713803595b933
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-02-25 17:57:21 +0300

    Переводит страницу ненайденного контента

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -10,41 +10,20 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'stairways' ); ?></h1>
-	</header><!-- .page-header -->
-
 	<div class="page-content">
 		<?php
-		if ( is_home() && current_user_can( 'publish_posts' ) ) :
-
-			printf(
-				'<p>' . wp_kses(
-					/* translators: 1: link to WP admin new post page. */
-					__( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'stairways' ),
-					array(
-						'a' => array(
-							'href' => array(),
-						),
-					)
-				) . '</p>',
-				esc_url( admin_url( 'post-new.php' ) )
-			);
-
-		elseif ( is_search() ) :
+		if ( is_search() ) :
 			?>
 
-			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'stairways' ); ?></p>
+			<p><?php esc_html_e( 'Ничего не нашли по вашему запросу', 'stairways' ); ?></p>
 			<?php
 			get_search_form();
 
 		else :
 			?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'stairways' ); ?></p>
+			<p><?php esc_html_e( 'Пока тут пусто, скоро мы наполним этот раздел', 'stairways' ); ?></p>
 			<?php
-			get_search_form();
-
 		endif;
 		?>
 	</div><!-- .page-content -->
