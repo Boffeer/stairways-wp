fcb7323d575901d63f19f75f51b5ac70d79bacad
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-03-01 12:02:41 +0300

    Выводит выбранные категории на главной

--- page-home.php
+++ page-home.php
@@ -14,72 +14,72 @@
 ?>
 <?php get_header(); ?>
   <main class="main">
+      <?php
+              $hero_categories = carbon_get_post_meta(get_the_ID(), 'home_categories');
+              $hero_categories_ids = array();
+              
+              foreach ($hero_categories as $category) :
+                  $hero_categories_ids[] = $category['id'];
+              endforeach;
+      ?>
+      <?php if (!empty($hero_categories_ids)) : ?>
       <div class="hero-categories">
           <div class="container hero-categories__container">
-              <div class="hero-categories__gallery">
-                  <article class="hero-categories-card hero-categories-card--current">
-                      <picture class="hero-categories-card__pic">
-                          <img src="<?php echo THEME_STATIC; ?>/img/featured/category.jpg" alt="" class="hero-categories-card__img">
-                      </picture>
+            <?php
+
+              $hero_categories = get_terms(array(
+                  'orderby' => 'none',
+                  'taxonomy' => 'categories',
+                  'hide_empty'    => false,
+                  'include' => $hero_categories_ids,
+              ));
+            ?>
+            <div class="hero-categories__gallery">
+                <?php foreach ($hero_categories as $category_index => $category) : ?>
+                  <article class="hero-categories-card <?php echo $category_index === 0 ? 'hero-categories-card--current' : '';?>">
+                      <?php $category_image_id = carbon_get_term_meta($category->term_id, 'category_pic'); ?>
+                        <picture class="hero-categories-card__pic">
+                          <?php if ($category_image_id != '') : ?>
+                            <img
+                              src="<?php echo boffeer_get_image_url_by_id($category_image_id); ?>"
+                              alt="<?php echo $category->name; ?>"
+                              class="hero-categories-card__img"
+                            >
+                          <?php endif; ?>
+                        </picture>
                       <h3 class="hero-categories-card__title">
-                          <a href="#" class="hero-categories-card__link">Лестницы на металлокаркасе</a>
+                          <a href="<?php echo get_term_link($category->term_id)?>" class="hero-categories-card__link"><?php echo $category->name; ?></a>
                       </h3>
-                      <div class="hero-categories-card__subcategories">
-                          <a href="#" class="hero-categories-card__subcategory">Ломаный косоур</a>
-                          <picture class="hero-categories-card__subcategory-pic"><img src="<?php echo THEME_STATIC; ?>/img/catalog/catalog-1.jpg" alt="" class="hero-categories-card__subcategory-img"></picture>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур</a>
-                          <picture class="hero-categories-card__subcategory-pic"><img src="<?php echo THEME_STATIC; ?>/img/catalog/catalog-2.jpg" alt="" class="hero-categories-card__subcategory-img"></picture>
-                          <a href="#" class="hero-categories-card__subcategory">Тетива из листа</a>
-                          <picture class="hero-categories-card__subcategory-pic"><img src="<?php echo THEME_STATIC; ?>/img/catalog/catalog-3.jpg" alt="" class="hero-categories-card__subcategory-img"></picture>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур из листа</a>
-                          <picture class="hero-categories-card__subcategory-pic"><img src="<?php echo THEME_STATIC; ?>/img/catalog/catalog-4.jpg" alt="" class="hero-categories-card__subcategory-img"></picture>
-                      </div>
+                      <?php
+                        $child_categories = get_terms(array(
+                          'orderby' => 'none',
+                          'taxonomy' => 'categories',
+                          'hide_empty'    => false,
+                          'parent' => $category->term_id,
+                        ));
+                      ?>
+                      <?php if (!empty($child_categories)) : ?>
+                        <div class="hero-categories-card__subcategories">
+                            <?php foreach ($child_categories as $child) : ?>
+                              <?php $child_category_image_id = carbon_get_term_meta($child->term_id, 'category_pic'); ?>
+                              <?php if ($child_category_image_id != '') : ?>
+                                <a href="<?php echo get_term_link($child->term_id)?>" class="hero-categories-card__subcategory">Ломаный косоур</a>
+                                <picture class="hero-categories-card__subcategory-pic">
+                                  <img class="hero-categories-card__subcategory-img"
+                                    src="<?php echo boffeer_get_image_url_by_id($child_category_image_id); ?>"
+                                    alt="<?php echo $child->name; ?>"
+                                  >
+                                </picture>
+                              <?php endif; ?>
+                            <?php endforeach;?>
+                        </div>
+                      <?php endif; ?>
                   </article>
-                  <article class="hero-categories-card">
-                      <picture class="hero-categories-card__pic">
-                          <img src="<?php echo THEME_STATIC; ?>/img/featured/category.jpg" alt="" class="hero-categories-card__img">
-                      </picture>
-                      <h3 class="hero-categories-card__title">
-                          <a href="#" class="hero-categories-card__link">Лестницы на металлокаркасе</a>
-                      </h3>
-                      <div class="hero-categories-card__subcategories">
-                          <a href="#" class="hero-categories-card__subcategory">Ломаный косоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Тетива из листа</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур из листа</a>
-                      </div>
-                  </article>
-                  <article class="hero-categories-card">
-                      <picture class="hero-categories-card__pic">
-                          <img src="<?php echo THEME_STATIC; ?>/img/featured/category.jpg" alt="" class="hero-categories-card__img">
-                      </picture>
-                      <h3 class="hero-categories-card__title">
-                          <a href="#" class="hero-categories-card__link">Лестницы на металлокаркасе</a>
-                      </h3>
-                      <div class="hero-categories-card__subcategories">
-                          <a href="#" class="hero-categories-card__subcategory">Ломаный косоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Тетива из листа</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур из листа</a>
-                      </div>
-                  </article>
-                  <article class="hero-categories-card">
-                      <picture class="hero-categories-card__pic">
-                          <img src="<?php echo THEME_STATIC; ?>/img/featured/category.jpg" alt="" class="hero-categories-card__img">
-                      </picture>
-                      <h3 class="hero-categories-card__title">
-                          <a href="#" class="hero-categories-card__link">Лестницы на металлокаркасе</a>
-                      </h3>
-                      <div class="hero-categories-card__subcategories">
-                          <a href="#" class="hero-categories-card__subcategory">Ломаный косоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур</a>
-                          <a href="#" class="hero-categories-card__subcategory">Тетива из листа</a>
-                          <a href="#" class="hero-categories-card__subcategory">Монокосоур из листа</a>
-                      </div>
-                  </article>
-              </div>
+                <?php endforeach; ?>
+            </div>
           </div>
       </div>
+      <?php endif; ?>
 
       <section class="offer section">
           <div class="container offer__container">
