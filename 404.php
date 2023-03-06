55b1c30f2dd3fdd741fb61a5aac7249b143383be
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-03-06 18:35:17 +0300

    Добалвяет 404

--- 404.php
+++ 404.php
@@ -12,47 +12,41 @@ get_header();
 
 	<main id="primary" class="site-main">
 
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'stairways' ); ?></h1>
-			</header><!-- .page-header -->
+		<div class="container">
+			<section class="error-404 not-found">
+				<p class="error-number">404</p>
 
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'stairways' ); ?></p>
 
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'stairways' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$stairways_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'stairways' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$stairways_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
+				<div class="page-content">
+					<h1 class="offer__title heading_title"><?php esc_html_e( 'Упс... не нашли такой страницы', 'stairways' ); ?></h1>
+					<p class="offer__subtitle">Попробуйте вернуться на главную страницу</p>
+					<a href="<?php echo get_home_link(); ?>" class="button button--primary">Вернуться на главную</a>
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+		</div>
+		<style>
+			.error-404 .error-number {
+				text-align: center;
+				font-size: 20rem;
+				font-weight: 700;
+				line-height: 1;
+			}
+			@media (max-width: 600px) {
+				.error-404 .error-number {
+					font-size: 10rem;
+				}
+			}
+			.error-404 .page-content {
+				max-width: 600px;
+				margin-left: auto;
+				margin-right: auto;
+				display: flex;
+				text-align: center;
+				justify-content: center;
+				flex-direction: column;
+				padding-bottom: 5rem;
+			}
+		</style>	
 
 	</main><!-- #main -->
 
