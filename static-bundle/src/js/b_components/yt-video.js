fc6463791133ac6c2617931abd8d3b8682956178
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-04-19 17:54:19 +0300

    Делает библиотеку ленивых ютуб видео

--- static-bundle/src/js/b_components/yt-video.js
+++ static-bundle/src/js/b_components/yt-video.js
@@ -1,70 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   if (document.querySelector('.yt-video') == null) return;
 
-  // Load the IFrame Player API code asynchronously.
-  var tag = document.createElement("script");
-  tag.src = "https://www.youtube.com/player_api";
-  var firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-  // Replace the 'ytplayer' element with an <iframe> and
-
-  // YouTube player after the API code downloads.
-  var player;
-  function onYouTubePlayerAPIReady() {
-    const videoId = document.querySelector('.yt-video').dataset.ytId;
-    player = new YT.Player("yt-video-0", {
-      videoId: videoId,
-      events: {
-        onReady: onPlayerReady,
-        onStateChange: cahngePlayerState,
-      },
-      playerVars: {
-        autoplay: 1,
-        controls: 0,
-        showinfo: 0,
-        modestbranding: 1,
-        fs: 0,
-        cc_load_policy: 0,
-        iv_load_policy: 3,
-        autohide: 1,
-        playsinline: 1,
-        mute: 1,
-        loop: 1,
-        rel: 0,
-        version: 3,
-        origin: window.location.href,
-      },
-    });
-  }
-  window.b_ytPlayer = player;
-
-  function onPlayerReady(event) {
-    event.target.setVolume(40);
-    event.target.playVideo();
-  }
-
-  function cahngePlayerState(e) {
-    // show video when loaded instead of thumb
-    if (e.data == 1) {
-      document.querySelector(".yt-video__media").style.display = "block";
-    } else if (e.data == 0) {
-      // document.querySelector(".yt-video__media").remove();
-    }
-
-    // loop video
-    if (e.data === YT.PlayerState.ENDED) {
-      player.playVideo();
-    }
-  }   
-
   function initYtVideos(items) {
     items.forEach((video, index) => {
+      if (video.classList.contains('video--inited')) return;
+
       const ytId = video.dataset.ytId;
 
       const thumbs = getVideoThumb(ytId);
 
       makeVideoMarkup(video, index);
       setVideoThmbs(video, ytId);
+
+      video.classList.add('video--inited')
     })
   }
 
@@ -86,7 +34,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
       thumb,
       webp,
     }
-
+    video._ytVideo.id = id;
   }
 
   function makeVideoMarkup(video, index) {
@@ -118,6 +66,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     video.appendChild(mediaWrap);
 
     video._ytVideo = {
+      player: {},
       pic,
       webp: picWebp,
       img,
@@ -134,29 +83,77 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
       video._ytVideo.play = playButton;
     }
-
     video.classList.add('video--marked-up');
   }
 
-    // let video = card.querySelector(videoIframeClassName);
-    // // let play = card.querySelector($.playButtonClassName);
-    // video.setAttribute("data-src", ytVideoUrl);
-    // let videoClass = `js_video--${index}`;
-    // video.classList.add(videoClass);
-
-
-    // card.addEventListener("click", initVideo);
-
-  function initVideo(e) {
-    const card = e.target.closest('yt-video');
-    // const iframe = 
-    // console.log("yep");
-    // if (card.classList.contains("js_lazy-init"))
-    // card.classList.add("js_lazy-init");
-    // video.src = video.getAttribute("data-src");
-    // debugger;
-    // card.removeEventListener("click", initVideo);
+  function initPlayers(videos) {
+    videos.forEach(video => {
+      // const videoElementId = video.id;//`yt-video-${index}`
+      video._ytVideo.player = new YT.Player(video._ytVideo.videoFrame.id, {
+        videoId: video._ytVideo.id,
+        events: {
+          onReady: onPlayerReady,
+          onStateChange: cahngePlayerState,
+        },
+        playerVars: {
+          autoplay: 1,
+          controls: 0,
+          showinfo: 0,
+          modestbranding: 1,
+          fs: 0,
+          cc_load_policy: 0,
+          iv_load_policy: 3,
+          autohide: 1,
+          playsinline: 1,
+          mute: 1,
+          loop: 1,
+          rel: 0,
+          version: 3,
+          origin: window.location.href,
+        },
+      });
+    });
   }
 
-  initYtVideos(document.querySelectorAll('.yt-video'));
+  function onPlayerReady(event) {
+    const video = event.target.h.closest('.yt-video');
+    event.target.setVolume(40);
+    event.target.playVideo();
+      setTimeout(() => {
+        video.classList.add('yt-video--video-play');
+      }, 1000);
+  }
+
+  function cahngePlayerState(e) {
+    // console.log('player change state', e)
+    const video = e.target.h.closest('.yt-video');
+
+    // show video when loaded instead of thumb
+    if (e.data == 1) {
+      // document.querySelector(".yt-video__media").style.display = "block";
+    } else if (e.data == 0) {
+      // document.querySelector(".yt-video__media").remove();
+    }
+
+    // loop video
+    if (e.data === YT.PlayerState.ENDED) {
+      video._ytVideo.player.playVideo();
+    }
+  }   
+
+  const videos = document.querySelectorAll('.yt-video')
+  initYtVideos(videos);
+
+  // Load the IFrame Player API code asynchronously.
+  var tag = document.createElement("script");
+  tag.src = "https://www.youtube.com/player_api";
+  var firstScriptTag = document.getElementsByTagName("script")[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+  // Replace the 'ytplayer' element with an <iframe> and
+  // YouTube player after the API code downloads.
+  function onYouTubePlayerAPIReady() {
+    initPlayers(videos)
+  }
+  window.onYouTubePlayerAPIReady = onYouTubePlayerAPIReady;
 });
